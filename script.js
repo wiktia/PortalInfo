50b922ca145a411a7f865953cf1fffc2cd465927
Wiktoria Kerner <wkerner@student.agh.edu.pl>
2025-11-17 11:34:54 +0100

    interfejslogowaniairejstracji

--- script.js
+++ script.js
@@ -23,3 +23,49 @@ document.querySelector('.carousel-button.prev').addEventListener('click', prevSl
 
 // Automatyczne przewijanie
 setInterval(nextSlide, 7000);
+
+
+//LOGIKA MENU LOGOWANIA I REJSTRACJI
+// Elementy
+const loginBtn = document.querySelector(".header__cta-button");
+const overlay = document.getElementById("authOverlay");
+const modal = document.getElementById("authModal");
+
+const loginForm = document.getElementById("loginForm");
+const registerForm = document.getElementById("registerForm");
+
+const closeButtons = document.querySelectorAll(".auth-close");
+
+const showRegister = document.getElementById("showRegister");
+const showLogin = document.getElementById("showLogin");
+
+
+// Otwórz modal
+loginBtn.addEventListener("click", () => {
+  overlay.classList.remove("hidden");
+  modal.classList.remove("hidden");
+  loginForm.classList.remove("hidden");
+  registerForm.classList.add("hidden");
+});
+
+// Zamknij modal
+function closeModal() {
+  overlay.classList.add("hidden");
+  modal.classList.add("hidden");
+}
+
+closeButtons.forEach(btn => btn.addEventListener("click", closeModal));
+overlay.addEventListener("click", closeModal);
+
+// Przejście do rejestracji
+showRegister.addEventListener("click", () => {
+  loginForm.classList.add("hidden");
+  registerForm.classList.remove("hidden");
+});
+
+// Przejście do logowania
+showLogin.addEventListener("click", () => {
+  registerForm.classList.add("hidden");
+  loginForm.classList.remove("hidden");
+});
+
